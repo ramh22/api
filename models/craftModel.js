a70bfc041f7ae90a8d35a2a3e8111ef17aa7d30c
ramh22 <s4alpy@gmail.com>
2023-04-30 15:42:36 +0300

    update

---
+++ models/craftModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const slugify=require('slugify');
+//const validator=require('validator');
+const Order=require('./../models/orderModel');
+const User=require('./../models/userModel');
+
+const craftSchema=new mongoose.Schema(
+    {
+    name:{
+        type:String,
+        required:[true,'a craft must have a name'],//validate,
+        unique:true,
+        trim:true,
+         maxLength:[40,'less or equal 40 character'],
+         minLength:[3,'more or equal 3 character'],
+     
+        },
+    image: {
+          type: String,
+          required: true,
+        },
+    slug:String,
+    workers:Array,
+    orders:Array,
+    
+  },
+    {
+        timestamps:true
+        ,toJSON:{virtuals:true}},
+    {toObject:{virtuals:true}}
+    );
+    const Craft = mongoose.model('Craft',craftSchema);
+module.exports =Craft; 
